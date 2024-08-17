73757a6dee77c129ff87bee3f622156ad146d058
sayantan <riosayan123@gmail.com>
2024-08-17 17:30:52 +0530

    added chakra ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { Box } from "@chakra-ui/react"
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <Box
+    minH="100vh" bg="#0f0a19" color='gray.500' px={6} py={8}>
+     asd
+    </Box>
   )
 }
 
---
+++ src/components/CodeEditor.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box } from '@chakra-ui/react'
+
+function CodeEditor() {
+  return (
+    <Box>
+      
+    </Box>
+  )
+}
+
+export default CodeEditor
